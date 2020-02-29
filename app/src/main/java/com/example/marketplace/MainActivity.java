c297c3acb5c3231ca15922c963a27f20beea44d9
Andrew-1000 <andruambia95@gmail.com>
2020-02-29 21:44:38 +0300

    Caputures user input from MainActivity and sends to StartActivity.class

--- app/src/main/java/com/example/marketplace/MainActivity.java
+++ app/src/main/java/com/example/marketplace/MainActivity.java
@@ -1,10 +1,14 @@
-package com.example.selfawareness;
+package com.example.marketplace;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,8 +17,10 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    @BindView(R.id.edtusername) EditText username;
-    @BindView(R.id.btnProceed) Button proceed;
+    @BindView(R.id.edtusername) EditText mUserName;
+    @BindView(R.id.btnProceed) Button mProceed;
+    @BindView(R.id.yourname) TextView getGetYourName;
+
     private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
@@ -23,20 +29,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
-        proceed.setOnClickListener(this);
+        mProceed.setOnClickListener(this);
     }
-
-
     @Override
     public void onClick(View v) {
-        if (v == proceed) {
+        if (v == mProceed) {
             openActivity();
         }
 
     }
 
     private void openActivity() {
+        String mUsername = mUserName.getText().toString();
+        Log.d(TAG, mUsername);
         Intent intent = new Intent(MainActivity.this, StartActivity.class);
+        intent.putExtra("mUsername", mUsername);
         startActivity(intent);
+        Toast toast = Toast.makeText(MainActivity.this, "Welcome " + mUsername, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
     }
 }
--- app/src/main/java/com/example/marketplace/StartActivity.java
+++ app/src/main/java/com/example/marketplace/StartActivity.java
@@ -1,14 +1,56 @@
-package com.example.selfawareness;
+package com.example.marketplace;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class StartActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class StartActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.welcome) TextView mWelcome;
+    @BindView(R.id.sell) Button mSell;
+    @BindView(R.id.buy) Button mBuy;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        ButterKnife.bind(this);
+
+        mSell.setOnClickListener(this);
+        mBuy.setOnClickListener(this);
+
+        Intent intent = getIntent();
+        String musername = intent.getStringExtra("mUsername");
+        mWelcome.setText(String.format("%s%s%s", getString(R.string.welcome), getString(R.string.space), musername));
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mSell) {
+            openSellActivity();
+        } else if ( v == mBuy) {
+            openBuyActivity();
+        }
+    }
+
+    private void openBuyActivity() {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.jumia.co.ke/"));
+        startActivity(browserIntent);
+    }
+
+    private void openSellActivity() {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.jumia.co.ke/market-place/"));
+        startActivity(browserIntent);
+    }
+
+
 }
