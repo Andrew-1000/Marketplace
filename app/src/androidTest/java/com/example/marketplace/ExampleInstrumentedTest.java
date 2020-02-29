722fd32e1a35bb7c1da640b636e187967f6492ce
Andrew-1000 <andruambia95@gmail.com>
2020-02-29 22:49:47 +0300

    Commit any changes that have not been staged for commits

--- app/src/androidTest/java/com/example/marketplace/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/marketplace/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.selfawareness;
+package com.example.marketplace;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.selfawareness", appContext.getPackageName());
+        assertEquals("com.example.marketplace", appContext.getPackageName());
     }
 }
